94cb29b3c652e0c30858b5c2ec9cd28f53c60ab9
Vinoth V <vvinothtvijay@gmail.com>
2021-06-26 08:02:36 +0530

    started boilerplate

--- App.js
+++ App.js
@@ -1,21 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
+import * as MediaLibrary from 'expo-media-library'
+import { Video } from 'expo-av'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const { width, height } = Dimensions.get('window')
+
+const App = () => {
+    const videoRef = useRef(null)
+    const [video, setVideo] = useState(null)
+    const [isplaying, setisplaying] = useState(false)
+
+    useEffect(() => {
+        getVideos()
+    }, [])
+
+    const getVideos = async () => {
+        await MediaLibrary.requestPermissionsAsync()
+        await MediaLibrary.getAssetsAsync({
+            mediaType: 'video',
+        })
+            .then(res => setVideo(res.assets[1]))
+            .catch(e => console.log("Erro: ", e))
+    }
+
+    if(video !== null) {
+        console.log(video)
+    }
+
+    const playPause = () => {
+        setisplaying(old => !old)
+    }
+
+    return (
+        <View style={ss.screen}>
+            { video !== null ? (
+                <View styles={ss.videoContainer}>
+                    <View style={ss.playerScreen}>
+                        <Video
+                            ref={videoRef}
+                            isLooping
+                            source={{ uri: video.uri }}
+                            shouldPlay={isplaying}
+                            useNativeControls={true}
+                            resizeMode={"contain"}
+                            style={ss.video}
+                        />
+                    </View>
+                    <Button
+                        title={isplaying ? "Pause" : "play"}
+                        onPress = {playPause}
+                    />
+                </View>
+            ) : (
+                <View>
+                    <Text>No video</Text>
+                </View>
+            )}
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const ss = StyleSheet.create({
+    screen: {
+        width,
+        height,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    videoContainer: {
+        width,
+        height: 300,
+
+    },
+
+    playerScreen: {
+        width,
+        height: 200
+    },
+
+    video: {
+        width: 320,
+        height: 200,
+    }
 });
+
+export default App;
+
+// Needed form of the video object
+// Object {
+//         "creationTime": 1614860594263,
+//         "duration": 93.422,
+//         "filename": "VID_20210304_175139.mp4",
+//         "height": 480,
+//         "id": "304852",
+//         "uri": "file:///storage/emulated/0/DCIM/Camera/VID_20210304_175139.mp4",
+// },
---
+++ StyleGuide.md
@@ -0,0 +1,26 @@
+Vudio Style Guide
+=================
+
+colors
+------
+
+    * white-100 #fff
+    * black-100 #000
+    * black-50 rgba(0, 0, 0, 0.5)
+    * black-10 rgba(0, 0, 0, 0.1)
+    * purple-100 #1C5668
+
+
+font family
+-----------
+
+    * sserif       'sans-serif'
+    * r-well-bold  'Rockwell'
+    * mono-corsiva 'Monotype Corsiva'
+
+font sizes
+----------
+
+    * size1  30
+    * size2  20
+    * size3  15
